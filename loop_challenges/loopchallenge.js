8df8687fb7f95cd3c7451ca3cf79c57f25ae5e9d
CarlosG <carlosgaytan0517@gmail.com>
2021-10-19 22:06:56 -0700

    initial commit

--- loop_challenges/loopchallenge.js
+++ loop_challenges/loopchallenge.js
@@ -38,3 +38,36 @@ for (var times=1; times<=12; times++){
         product=product*times
     }
 }console.log(product)
+
+// WALKTHROUGH ANSWERS
+
+//Challenge 1
+for(var i=1; i<20; i+=2) {
+    console.log(i);
+}
+
+//Challenge 2
+for(var i=100; i>-1; i--) {
+    if(i % 3 == 0) {
+        console.log(i);
+    }
+}
+
+//Challenge 3
+for(var i=4; i>-4; i-=1.5) {
+    console.log(i);
+}
+
+//Challenge 4
+var sum = 0;
+for(var i=1; i<101; i++) {
+    sum += i;
+}
+console.log(sum);
+
+//Challenge 5
+var product = 1;
+for(var i=1; i<13; i++) {
+    product *= i;
+}
+console.log(product);
